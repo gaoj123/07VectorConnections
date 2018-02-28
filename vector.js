2cbf94a325135d0d68ec0a94c5898f59ee9e6a2e
Jenny Gao <jgao658@gmail.com>
2018-02-28 13:54:36 -0500

    can connect circles

---
+++ vector.js
@@ -0,0 +1,39 @@
+var clearButton=document.getElementById("clear")
+var slate=document.getElementById("screen");
+var nowX=0;
+var nowY=0;
+
+var started=false;
+
+var circle=function(e){
+    mouseX=e.offsetX;
+    mouseY=e.offsetY;
+    nowX=mouseX
+    nowY=mouseY
+    started=true;
+    var c=document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    c.setAttribute("cx",mouseX);
+    c.setAttribute("cy",mouseY);
+    c.setAttribute("r",10);
+    slate.appendChild(c)
+};
+
+var draw=function(e){
+    if (started){
+	mouseX=e.offsetX;
+	mouseY=e.offsetY;
+	var l=document.createElementNS("http://www.w3.org/2000/svg", "line");
+	l.setAttribute("x1",nowX);
+	l.setAttribute("y1",nowY);
+	l.setAttribute("x2",mouseX);
+	l.setAttribute("y2",mouseY);
+	l.setAttribute("stroke","black");
+	slate.appendChild(l)
+	circle(e);
+    }
+    else{
+	circle(e);
+    }
+};
+
+slate.addEventListener("click",draw);
